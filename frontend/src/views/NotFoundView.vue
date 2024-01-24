3132494b7076a0fd227937fe51c47d305516741c
Linden Crandall <lindencrandall123@gmail.com>
2024-01-24 10:31:27 +0900

    404 handling

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="text-is-light not-found-container" style="padding-bottom: 25rem;">
+        <iframe 
+            src="https://giphy.com/embed/agmheddabICHK" 
+            width="480" 
+            height="360" 
+            frameBorder="0" 
+            allowFullScreen>
+        </iframe>
+        <div style="padding-top: 2rem;">
+            <a href="/" :class=$store.state.theme_change style="font-size: 20px; focus">
+                Click here to return to homepage
+            </a>
+        </div>
+
+    </div>
+
+</template>
+
+<style type="text/css">
+a:hover{
+    color: rgb(130, 130, 130);
+}
+</style>
+    
+<script>
+export default {
+
+    data() {
+        return {
+        }
+    },
+
+    mounted() {
+        document.title = '404 Not Found';
+
+    },
+
+}
+</script>
