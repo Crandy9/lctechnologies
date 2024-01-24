e7adb7404a5b1299c430f60b6cc6102dc45e21d5
Linden Crandall <lindencrandall123@gmail.com>
2024-01-24 10:31:10 +0900

    state stuff

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -9,6 +9,13 @@ export default createStore({
     isDarkTheme: false,
     theme_change: "lightTheme",
     theme: '',
+    bgColorDark: 'rgb(22 32 47)',
+    bgColorLight: '#f2fafb',
+    scrollingToEcommerce: false,
+    scrollingToWebApp: false,
+    scrollingToContact: false,
+
+
 
   },
   getters: {
@@ -17,10 +24,23 @@ export default createStore({
   },
   mutations: {
 
+    scrollTo(state, section) {
+
+      if (section === "ecommerce") {
+        state.scrollingToEcommerce = true;
+      }
+      else if (section === "webapp") {
+        state.scrollingToWebApp = true;
+      }
+      else if (section === "contact") {
+        state.scrollingToContact = true;
+      }
+    },
+
     toggleTheme(state) {
 
         state.isDarkTheme = !state.isDarkTheme;
-        document.body.style.backgroundColor = state.isDarkTheme ? 'rgb(50, 50, 50)' : 'white';
+        document.body.style.backgroundColor = state.isDarkTheme ? state.bgColorDark : state.bgColorLight;
         state.theme_change = state.isDarkTheme ? "darkTheme" : "lightTheme";
   
         localStorage.setItem("theme", state.theme_change);
@@ -31,14 +51,14 @@ export default createStore({
 
       if (theme === 'darkTheme') {
         state.isDarkTheme = true
-        document.body.style.backgroundColor = 'rgb(50, 50, 50)'
+        document.body.style.backgroundColor = state.bgColorDark
         state.theme_change = "darkTheme"
         localStorage.setItem("theme", state.theme_change);
       }
 
       else if (theme === 'lightTheme') {
         state.isDarkTheme = false
-        document.body.style.backgroundColor = 'white'
+        document.body.style.backgroundColor = state.bgColorLight
         state.theme_change = "lightTheme"
         localStorage.setItem("theme", state.theme_change);
       }
