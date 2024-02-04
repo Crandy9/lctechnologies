2b3d55205084f82515450445f3109d9527fda022
Linden Crandall <lindencrandall123@gmail.com>
2024-02-04 10:11:39 +0900

    SMTP implementation

---
+++ backend/form_data/urls.py
@@ -0,0 +1,13 @@
+# manually created urls.py file
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from form_data import views
+
+
+urlpatterns = [
+    path('form-data/', views.process_form_data),
+] 
+
---
+++ backend/form_data/views.py
@@ -0,0 +1,85 @@
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from django.core.mail import EmailMessage
+# converts html template to a string message for emails
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from django.conf import settings
+from lctec_project import settings
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+from rest_framework import permissions, status
+from django.middleware.csrf import get_token
+from django.http import JsonResponse
+# for saving profilepic
+from PIL import Image
+from django.core.files import File
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from rest_framework.permissions import AllowAny
+
+# send thank you email to user after purchase
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+# set up logger
+import logging
+import os
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create file handler and set level to INFO
+log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lctec_user_logs.txt')
+fh = logging.FileHandler(log_file)
+fh.setLevel(logging.INFO)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+
+user = get_user_model()
+
+
+EMAIL_ON = True
+
+@api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
+def process_form_data(request):
+
+    template = render_to_string('../templates/cust_alert.html', {
+        'cust_name': request.data.get('name', ''),
+        'cust_email': request.data.get('email', ''),
+        'service': request.data.get('service', ''),
+        'details': request.data.get('msgs', ''),
+    })  
+    # send thankyou email to user
+    email = EmailMessage(
+        # email subject title default is 'subject'
+        'CUSTOMER ALERT',
+        # email template default is 'body'
+        template,
+        settings.EMAIL_HOST_USER,
+        # recipient list
+        # [request.user.email],
+        [settings.env('ADMIN_EMAIL')],
+    )
+    email.fail_silently=False
+    # only send email if this flag is true
+    if EMAIL_ON:
+        email.send()
+
+
+    return Response(status=200)
