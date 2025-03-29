55bbdc1dec567cfcb84c165b0443d70d3197f52d
Linden Crandall <lindencrandall123@gmail.com>
2025-03-30 01:12:43 +0900

    replaced moon and sun icons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -28,8 +28,10 @@
         <div class="navbar-item my-theme-switch">
           <button @click="toggleTheme()" class="theme-toggle">
               <div>
-                <i v-if="$store.state.isDarkTheme" class="gg-moon theme-icon-when-dark"></i>
-                <i v-else-if="!$store.state.isDarkTheme" class="gg-sun theme-icon-when-light"></i>
+                <i v-if="$store.state.isDarkTheme" class="gg-moon fa fa-moon theme-icon-when-dark"></i>
+                <svg v-else-if="!$store.state.isDarkTheme" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="gg-sun theme-icon-when-light" viewBox="0 0 16 16">
+                  <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
+                </svg>
               </div>
           </button>
         </div>
@@ -382,8 +384,6 @@
   @import '../src/assets/styles/my-theme.css';
   @import '../src/assets/styles/my-landing.css';
   @import '../src/assets/styles/my-services.css';
-  @import url('https://unpkg.com/css.gg@2.0.0/icons/css/moon.css');
-  @import url('https://unpkg.com/css.gg@2.0.0/icons/css/sun.css');
 </style>
 
 
